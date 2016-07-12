0b13156107f4d24787bcb5f96221f11e5f82551d
Brian Kim <bk752@github.com>
2016-07-11 21:27:56 -0700

    Add stuff hydrophones
    lol jk

--- LabVIEW Data/Dunkpahones/FPGA Bitfiles/NiFpga_DunkFlips.h
+++ LabVIEW Data/Dunkpahones/FPGA Bitfiles/NiFpga_DunkFlips.h
@@ -24,42 +24,38 @@
 /**
  * The signature of the FPGA bitfile.
  */
-static const char* const NiFpga_DunkFlips_Signature = "75649797EE328EB749373B91A56B085D";
+static const char* const NiFpga_DunkFlips_Signature = "4A5FF50DBBFE15633C248D80810F5ACD";
 
 typedef enum
 {
-   NiFpga_DunkFlips_IndicatorBool_InvalidPeriod = 0x6E,
-} NiFpga_DunkFlips_IndicatorBool;
-
-typedef enum
-{
-   NiFpga_DunkFlips_IndicatorI32_Output1 = 0x38,
-   NiFpga_DunkFlips_IndicatorI32_Output2 = 0x44,
-   NiFpga_DunkFlips_IndicatorI32_Output3 = 0x14,
+   NiFpga_DunkFlips_IndicatorI32_Output1 = 0x3C,
+   NiFpga_DunkFlips_IndicatorI32_Output2 = 0x48,
+   NiFpga_DunkFlips_IndicatorI32_Output3 = 0x18,
    NiFpga_DunkFlips_IndicatorI32_Output4 = 0x10,
 } NiFpga_DunkFlips_IndicatorI32;
 
 typedef enum
 {
-   NiFpga_DunkFlips_IndicatorU32_ActualSamplePeriodProcessticks = 0x2C,
+   NiFpga_DunkFlips_IndicatorU32_ActualSamplePeriodProcessticks = 0x30,
    NiFpga_DunkFlips_IndicatorU32_ActualSamplePeriodWaveticks = 0x70,
 } NiFpga_DunkFlips_IndicatorU32;
 
 typedef enum
 {
-   NiFpga_DunkFlips_ControlBool_HighPass = 0x1A,
-   NiFpga_DunkFlips_ControlBool_LowPass = 0x1E,
+   NiFpga_DunkFlips_ControlBool_HighPass = 0x1E,
+   NiFpga_DunkFlips_ControlBool_LowPass = 0x22,
 } NiFpga_DunkFlips_ControlBool;
 
 typedef enum
 {
-   NiFpga_DunkFlips_ControlU32_Minsignaltimegapsamples = 0x30,
-   NiFpga_DunkFlips_ControlU32_RequestedSamplePeriodticks = 0x68,
+   NiFpga_DunkFlips_ControlU32_FIFOSignal = 0x14,
+   NiFpga_DunkFlips_ControlU32_Minsignaltimegapsamples = 0x34,
+   NiFpga_DunkFlips_ControlU32_RequestedSamplePeriodticks = 0x6C,
 } NiFpga_DunkFlips_ControlU32;
 
 typedef enum
 {
-   NiFpga_DunkFlips_TargetToHostFifoI32_Output = 0,
+   NiFpga_DunkFlips_TargetToHostFifoI32_Output = 1,
 } NiFpga_DunkFlips_TargetToHostFifoI32;
 
 #endif
